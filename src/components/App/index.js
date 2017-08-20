5c2fcb7c67537c8643a8f5abbb514c2c7640e093
Ting Chen <ting@squareup.com>
2017-08-20 16:51:53 -0700

    add babel-module-resolver. Notes: babelrc lives inside package.json

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from 'assets/logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          test
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TING</h1>
+        <div>
+          Finally getting something going woooo
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
