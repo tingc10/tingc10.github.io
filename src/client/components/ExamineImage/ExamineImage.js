dbd8ceae33ebe1e76ac0e228b237dc89f70fe974
Ting Chen <ting@squareup.com>
2018-04-07 21:28:04 -0700

    Animate in examined image after clicking on filmstrip

--- src/client/components/ExamineImage/ExamineImage.js
+++ src/client/components/ExamineImage/ExamineImage.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.sass';
 import Image from 'Image/Image';
+import { TimelineMax, Power2 } from 'gsap';
+
+const playTimeline = (timeline) => {
+  return new Promise(((resolve) => {
+    timeline.eventCallback('onComplete', () => resolve('Animation Complete'));
+    timeline.play();
+  }));
+}
 
 export default class ExamineImage extends Component {
   static propTypes = {
@@ -19,7 +27,31 @@ export default class ExamineImage extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
+    if (this.props.image !== prevProps.image) {
+      this.loadNewImage();
+    }
+  }
 
+  loadNewImage = () => {
+    this.fadeOutPrevImage().then(() => {
+      this.setState({
+        image: this.props.image
+      }, this.fadeInCurrentImage);
+    });
+  }
+
+  fadeOutPrevImage = () => {
+    const timeline = new TimelineMax({paused: true});
+    const imageRef = this.imageRef.refs.image;
+    timeline.to(imageRef, 0.3, { scale: 0.8, autoAlpha: 0, ease: Power2.easeOut } );
+    return playTimeline(timeline);
+  }
+
+  fadeInCurrentImage = () => {
+    const timeline = new TimelineMax({paused: true});
+    const imageRef = this.imageRef.refs.image;
+    timeline.to(imageRef, 0.5, { scale: 1, autoAlpha: 1, ease: Power2.easeOut } );
+    return playTimeline(timeline);
   }
 
   render() {
@@ -31,7 +63,7 @@ export default class ExamineImage extends Component {
     };
     return (
       <div className={styles.imageContainer}>
-        <Image {...imageProps} />
+        <Image ref={(node) => { this.imageRef = node; }} {...imageProps} />
       </div>
     );
   }
