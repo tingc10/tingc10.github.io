84a079dd678683e1c7391925b1d41293bb677d6b
Ting Chen <ting@squareup.com>
2018-09-03 12:32:17 -0700

    Main page fixed

--- src/client/components/Home/Home.js
+++ src/client/components/Home/Home.js
@@ -6,14 +6,16 @@ class Home extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <div className={styles.left}>
-          <AnimatedLogo />
-        </div>
-        <div className= {styles.right}>
-          <h1 className={styles.header}>Welcome</h1>
-          <p className={styles.intro}>
-            My name is Ting and I'm an UI Engineer. Way back when, I used to dabble in art and design. Since then, I've picked up a few new skills and turned my attention towards building delightful web experiences.
-          </p>
+        <div className={styles.position}>
+          <div className={styles.left}>
+            <AnimatedLogo />
+          </div>
+          <div className= {styles.right}>
+            <h1 className={styles.header}>Welcome</h1>
+            <p className={styles.intro}>
+              My name is Ting and I'm an UI Engineer. Way back when, I used to dabble in art and design. After earning my CSE degree at Michigan, I've since turned my attention towards building delightful web experiences.
+            </p>
+          </div>
         </div>
       </div>
     );
