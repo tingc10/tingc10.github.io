a330b6243b11a6290fa2b39fb079f1ec3f263c99
Ting Chen <tingshen.chen@gmail.com>
2020-02-21 01:42:06 -0500

    Updates

---
+++ precache-manifest.b9f6f4ff635b178b80d4af24b4a0fb94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1e8637fe43c3d2f8c756f850f35670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658596ddeebcbbd7f962",
+    "url": "/static/css/main.7978a3f3.chunk.css"
+  },
+  {
+    "revision": "2a7c950f3d0278410a8a",
+    "url": "/static/js/2.8b4fa814.chunk.js"
+  },
+  {
+    "revision": "6a949d7fc503ca0d6fceff9ba62b492c",
+    "url": "/static/js/2.8b4fa814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658596ddeebcbbd7f962",
+    "url": "/static/js/main.1dd8b2be.chunk.js"
+  },
+  {
+    "revision": "5db6dfe7a8e2b4135dd2",
+    "url": "/static/js/runtime-main.aa739829.js"
+  },
+  {
+    "revision": "41b024268a9fd78fe73178655cc88e3f",
+    "url": "/static/media/ting-trail.41b02426.svg"
+  }
+]);
