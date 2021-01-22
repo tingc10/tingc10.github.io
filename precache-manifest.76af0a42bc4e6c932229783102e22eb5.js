5b21bb6158fdf44289fbcf7404898be92b3b4ac1
Ting Chen <tingshen.chen@gmail.com>
2021-01-22 00:02:53 -0500

    Updates

--- precache-manifest.76af0a42bc4e6c932229783102e22eb5.js
+++ precache-manifest.76af0a42bc4e6c932229783102e22eb5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fcef74838a097e2cf43ce9351dc0a5fc",
+    "revision": "83ec4e3e4d4f97a4b05466eecfb11a19",
     "url": "/index.html"
   },
   {
-    "revision": "a9efd6eb9160c6349537",
-    "url": "/static/css/main.4ddc962b.chunk.css"
+    "revision": "9b38c52be926035f599b",
+    "url": "/static/css/main.3879aac2.chunk.css"
   },
   {
-    "revision": "1537c2e05107cd0668e8",
-    "url": "/static/js/2.e8ea9fae.chunk.js"
+    "revision": "41b92d51a19a7d4bcd80",
+    "url": "/static/js/2.8e2b1df0.chunk.js"
   },
   {
     "revision": "6a949d7fc503ca0d6fceff9ba62b492c",
-    "url": "/static/js/2.e8ea9fae.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8e2b1df0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a9efd6eb9160c6349537",
-    "url": "/static/js/main.bca1cc42.chunk.js"
+    "revision": "9b38c52be926035f599b",
+    "url": "/static/js/main.75223840.chunk.js"
   },
   {
     "revision": "5db6dfe7a8e2b4135dd2",
